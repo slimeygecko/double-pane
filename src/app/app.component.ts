fa2f33cd5759a02b95f9ff8ed2c511b6ab1c9973
Mike Woodward <michaelcwoodward@gmail.com>
2017-09-15 00:30:47 -0500

    Creating the overlay / landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+const defaultPane = {
+  type: 'letters'
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  panes = [{ type: 'letters'}, { type: 'numbers'}];
+  panes = [ defaultPane ];
+  isSinglePane = true;
+  isOverlayOpen = true;
+
+  onStart = function() {
+    this.isOverlayOpen = false;
+  };
+
+  onSelectPanes = function(value) {
+    const isSingle = value === 'single';
+
+    this.isSinglePane = isSingle;
+
+    this.panes = isSingle ?
+      [ defaultPane ] :
+      [
+        defaultPane,
+        defaultPane
+      ];
+  };
 }
